c0700689f86f1c7f8ad1664159935bde18c7ea09
Dhaniayee <58738668+Dhaniayee@users.noreply.github.com>
2019-12-10 22:18:41 +0700

    Update skrt.php

--- skrt.php
+++ skrt.php
@@ -2,13 +2,15 @@
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
 echo color("red"," ===========================\n");
-echo color("green","| Claim Voucher Gojek|\n");
+echo color("red","| Claim Voucher Gojek|\n");
 echo color("red","| Auto create Gojek X Redeem voucher |\n");
-echo color("green","| github  : Dhaniayee |\n");
-echo color("red","| Creator : Mas Ayee         |\n");
+echo color("red","| github: kumpulanremaja |\n");
+echo color("red","| fanspage :4kumpulanremaja|\n");
+echo color("red","| Creator : kumpulanremaja.com          |\n");
 echo "| Version : Premium X pertalite      |\n";
 echo "| Time    :".date('[d-m-Y] [H:i:s]')."   |\n";
 echo " ===========================\n";
+
 // function change(){
         $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
@@ -21,6 +23,7 @@ echo " ===========================\n";
         $nohp = str_replace(")","",$nohp);
         $nohp = str_replace("-","",$nohp);
         $nohp = str_replace(" ","",$nohp);
+
         if (!preg_match('/[^+0-9]/', trim($nohp))) {
             if (substr(trim($nohp),0,3)=='62') {
                 $hp = trim($nohp);
@@ -183,4 +186,5 @@ echo " ===========================\n";
          goto ulang;
          }
 //  }
+
 // echo change()."\n";
